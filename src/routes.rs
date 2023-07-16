ed6d925f176ad77cabbe9d969ee19153ec24d531
benthecarman <benthecarman@live.com>
2023-07-16 00:57:29 -0500

    remove comment

--- src/routes.rs
+++ src/routes.rs
@@ -112,9 +112,6 @@ pub(crate) fn set_user_config_impl(
     if !valid_emoji_string(&emoji_str) {
         return Err(anyhow::anyhow!("Invalid emoji: {emoji_str}"));
     }
-    // if emoji::lookup_by_glyph::contains_glyph(&emoji_str) {
-    //     return Err(anyhow::anyhow!("Invalid emoji: {emoji_str}"));
-    // }
 
     let npub = payload.npub;
     match payload.into_db() {
